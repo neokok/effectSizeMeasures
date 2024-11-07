71b4330b06cdc48ab5e9c77ca100c75a74b501f9
Neo Kok <112899144+neokok@users.noreply.github.com>
2024-11-07 09:01:24 -0500

    Test

--- R/add.R
+++ R/add.R
@@ -1,3 +1,12 @@
+#' Add the inputs together.
+#'
+#' This function takes two numbers and returns the sum.
+#'
+#' @param x A numeric value to be added.
+#' @param y A numeric value to be added.
+#' @return The input values summed together.
+#' @export
+
 add = function(x, y){
   return(x + y)
 }
