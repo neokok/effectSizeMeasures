4aec943a39c20edf7d72d4c7765b7c489048b216
Neo Kok <112899144+neokok@users.noreply.github.com>
2024-11-12 13:45:52 -0500

    Risk Ratio bug fix

--- R/risk_ratio.R
+++ R/risk_ratio.R
@@ -1,9 +1,9 @@
-#' Function to compute risk ratio (relative risk).
+#' Function to compute risk ratio (relative risk) with Wald Approximation.
 #'
 #' This function takes a 2x2 matrix and calculates the risk ratio. The formula for this is:
 #' \deqn{\text{Risk ratio} = \frac{\text{Diseased}_{\text{Exposed}} / (\text{Diseased}_{\text{Exposed}} + \text{NonDiseased}_{\text{Exposed}})}{\text{Diseased}_{\text{NonExposed}} / (\text{Diseased}_{\text{NonExposed}} + \text{NonDiseased}_{\text{NonExposed}})}}
 #'
-#' @param data A 2x2 numeric matrix of the form: c(Diseased_Exposed, NonDiseased_Exposed, Disease_NonExposed, NonDisease_NonExposed2)
+#' @param data A 2x2 numeric matrix of the form: as.matrix(c(Diseased_Exposed, NonDiseased_Exposed, Disease_NonExposed, NonDisease_NonExposed2))
 #' @return A numeric value of the risk ratio of the matrix.
 #' @examples
 #' risk_ratio(matrix(c(20, 6, 380, 594), nrow = 2, ncol = 2))
@@ -19,7 +19,7 @@ risk_ratio = function(data){
   c = data[2,1]
   d = data[2,2]
 
-  risk = (a / (a + b)) / (c / (c + d))
+  risk = (d / (d + c)) / (b / (a + b))
   if(!is.infinite(risk)){
     return(risk)
   }
